30d2ed979b48d57080b0f902a5818fa2498d451f
Marco Kaul <marco.kaul@ueberbit.de>
2016-05-06 11:14:33 +0200

    Add source map passthrough

--- index.js
+++ index.js
@@ -2,8 +2,29 @@
 	MIT License http://www.opensource.org/licenses/mit-license.php
 	Author Tobias Koppers @sokra
 */
-module.exports = function(content) {
+
+var path = require('path');
+
+module.exports = function(content, map) {
 	this.cacheable && this.cacheable();
 	this.value = content;
-	return "module.exports = " + JSON.stringify(content);
+
+	if(map) {
+    if (typeof map === 'string') {
+      map = JSON.parse(map);
+    }
+
+    if(map.sources) {
+      map.sources = map.sources.map(function(source) {
+        source = source.split("!").pop();
+        var p = path.relative(this.options.context, source).replace(/\\/g, "/");
+        if(p.indexOf("../") !== 0)
+          p = "./" + p;
+        return "/" + p;
+      }, this);
+      //map.sourceRoot = "webpack://";
+    }
+  }
+
+	return "module.exports = [[module.id, " + JSON.stringify(content) + ", \"\", " + JSON.stringify(map) + "]];";
 }
