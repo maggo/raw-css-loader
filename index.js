c5b7ee514448add29e5b86e3e0d3b2793982f187
Marco Kaul <marco.kaul@ueberbit.de>
2016-05-06 11:18:04 +0200

    Spaces to tabs

--- index.js
+++ index.js
@@ -10,21 +10,21 @@ module.exports = function(content, map) {
 	this.value = content;
 
 	if(map) {
-    if (typeof map === 'string') {
-      map = JSON.parse(map);
-    }
+		if (typeof map === 'string') {
+			map = JSON.parse(map);
+		}
 
-    if(map.sources) {
-      map.sources = map.sources.map(function(source) {
-        source = source.split("!").pop();
-        var p = path.relative(this.options.context, source).replace(/\\/g, "/");
-        if(p.indexOf("../") !== 0)
-          p = "./" + p;
-        return "/" + p;
-      }, this);
-      //map.sourceRoot = "webpack://";
-    }
-  }
+		if(map.sources) {
+			map.sources = map.sources.map(function(source) {
+				source = source.split("!").pop();
+				var p = path.relative(this.options.context, source).replace(/\\/g, "/");
+				if(p.indexOf("../") !== 0)
+					p = "./" + p;
+				return "/" + p;
+			}, this);
+			//map.sourceRoot = "webpack://";
+		}
+	}
 
 	return "module.exports = [[module.id, " + JSON.stringify(content) + ", \"\", " + JSON.stringify(map) + "]];";
 }
